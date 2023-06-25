f63d0be278720d1a5111592dafea75c62f99cfbf
Zh31n <zh31n@mail.com>
2023-06-25 12:36:49 +0300

    add changes in footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,39 +4,40 @@ import telegram from '../../assets/telegram.svg'
 import whatsapp from '../../assets/whatsapp.svg'
 
 const Footer = () => {
-  return (
-    <div className={s.main_container}>
-      <div className={s.form_container}>
-        <form>
-          <label className={s.text}>
-            Свяжитесь и получите консультацию!
-          </label>
-          <div className={s.label_container}>
-            <label>Ваш номер телефона</label>
-            <input placeholder="Ваш номер" />
-          </div>
-          <div className={s.label_container}>
-            <label>Ваше имя</label>
-            <input placeholder="Ваше имя" />
-          </div>
-            <button className={s.btn}>Отправить</button>
-        </form>
-      </div>
-      <div className={s.about}>
-          <div style={{display:'flex',gap:'1rem'}}>
-              <a href="">
-                  <img src={telegram} style={{height:'40px',width:'40px'}} alt=""/>
-              </a>
-              <a href="">
-                  <img src={whatsapp} style={{height:'40px',width:'40px'}} alt=""/>
-              </a>
-          </div>
-          <div className="">
+    return (
+        <div className={s.main_container}>
+            <div className={s.form_container}>
+                <form>
+                    <label className={s.text}>
+                        Свяжитесь и получите консультацию!
+                    </label>
+                    <div className={s.label_container}>
+                        <label>Ваш номер телефона</label>
+                        <input placeholder="Ваш номер"/>
+                    </div>
+                    <div className={s.label_container}>
+                        <label>Ваше имя</label>
+                        <input placeholder="Ваше имя"/>
+                    </div>
+                    <button className={s.btn}>Отправить</button>
+                </form>
+            </div>
+            <div className={s.about}>
+                <div className="">
+                    +7 (800)201-82-27
+                </div>
+                <div style={{display: 'flex', gap: '1rem', justifyContent: 'space-around', marginTop: '10px'}}>
+                    <a href="https://t.me/+79052477324">
+                        <img src={telegram} style={{height: '40px', width: '40px'}} alt=""/>
+                    </a>
+                    <a href="https://wa.me/79052477324">
+                        <img src={whatsapp} style={{height: '40px', width: '40px'}} alt=""/>
+                    </a>
+                </div>
 
-          </div>
-      </div>
-    </div>
-  );
+            </div>
+        </div>
+    );
 };
 
 export default Footer;
