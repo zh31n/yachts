7a6844a5b2a1f6a30e3f768ad55ec63f98354eae
Zh31n <zh31n@mail.com>
2023-07-18 09:19:35 +0300

    add footer fix and link fix

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import s from "./Header.module.css";
+import s from "./Header.module.scss";
 import Logo from "../../assets/logo.svg";
 import { Link } from "react-router-dom";
 import Tel from "../../assets/tel_icon.svg";
@@ -13,7 +13,7 @@ const Header = props => {
         <div className={"container"}>
           <div className={s.h_i}>
             <div className={s.container}>
-              <p className={s.text}>+7(800) 201 82-27</p>
+              <a href={'+78002018227'} className={s.text}>+7(800) 201 82-27</a>
               <img className={s.logo} src={Logo} alt={"logo"} />
             </div>
             <div className={s.nav}>
@@ -35,7 +35,7 @@ const Header = props => {
                 src={Tel}
                 alt="telephone icon"
               />
-              <span>+7(800) 201 82-27</span>
+              <a href={'tel:+78002018227'}>+7(800) 201 82-27</a>
             </div>
             <div
               className={s.navM}
